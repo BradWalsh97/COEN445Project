d525e8ad376ea6876c3b66d783b19b3d50e5b8fb
Brad Walsh <bradley.walsh.97@gmail.com>
2020-11-18 00:33:05 -0500

    added easy way to change subjects

---
+++ client/src/main/java/com/coen445/FinalProject/Subjects.java
@@ -0,0 +1,9 @@
+package com.coen445.FinalProject;
+
+public abstract class Subjects {
+    public static final String INTEREST1 = "1. Politics";
+    public static final String INTEREST2 = "2. Sports";
+    public static final String INTEREST3 = "3. Pop culture";
+    public static final String INTEREST4 = "4. Weather";
+    public static final String INTEREST5 = "5. Technology";
+}
