493ec184db6534271956dcb3c2bc5ea3042c2550
Brad Walsh <bradley.walsh.97@gmail.com>
2020-11-17 22:18:46 -0500

    Auto stash before merge of "messagePassing" and "origin/messagePassing"

--- server/src/main/java/com/coen445/FinalProject/ClientHandlerClass.java
+++ server/src/main/java/com/coen445/FinalProject/ClientHandlerClass.java
@@ -2,17 +2,17 @@ package com.coen445.FinalProject;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 
-import java.io.*;
-import java.net.Socket;
+import java.io.EOFException;
+import java.io.IOException;
 import java.net.SocketException;
 
 //todo: in client make sure that socket is unique. if not unique, chose another random socket until a free one is found
 //todo: while also making sure that they're above the reserved sockets [(thus do rand() % (max socket - amount of reserved sockets)] + amount of reserved sockets
 
-public class ClientHandlerClass extends Thread{
+public class ClientHandlerClass extends Thread {
     private Server server;
 
-    public ClientHandlerClass(Server server){
+    public ClientHandlerClass(Server server) {
         this.server = server;
     }
 
@@ -25,8 +25,9 @@ public class ClientHandlerClass extends Thread{
         RQ receivedRQ = null;
         System.out.println("Request on port: " + server.getPort());
 
-        loop: while(true){
-            if(!server.getRegistered()){
+        loop:
+        while (true) {
+            if (!server.getRegistered()) {
                 try {
                     toReturn = "TOREGISTER";
                     server.sendObject(toReturn);
@@ -34,8 +35,7 @@ public class ClientHandlerClass extends Thread{
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-            else{
+            } else {
                 //spit the received message. Each part of the frame is separated by a space. Thus
                 //the type of message will be the first element.
                 try {
@@ -44,41 +44,76 @@ public class ClientHandlerClass extends Thread{
                     //in the event a client randomly disconnects, it will throw and end of file exception.
                     //When this happens, we're going to catch it, print the log that says a user disconnected, and then move on
                     //if(e.equals(EOFException.class)){
-                    if(e instanceof EOFException || e instanceof SocketException){
+                    if (e instanceof EOFException || e instanceof SocketException) {
                         System.out.println("A user disconnected while server waiting to receive a message");
                         break;
-                    }
-                    else
+                    } else
                         e.printStackTrace();
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
                 //String[] messageSegments = received.split(" ");
-                message = (byte[])received;
+                message = (byte[]) received;
                 try {
                     receivedRQ = new RQ(message);
                 } catch (InvalidProtocolBufferException e) {
                     e.printStackTrace();
                 }
                 switch (receivedRQ.getRegisterCode()) {
-                    case 0:
+                    case 0: //register
                         try {
+                            //start by receiving the message and logging its info
                             System.out.println("Registered new user " + receivedRQ.getName() + " " + receivedRQ.getIp() + " " + receivedRQ.getSocketNum());
-                            server.sendObject("REGISTERED");
-                            RQ returnRQ = new RQ(1, receivedRQ.getRqNum());
-                            server.sendObject(returnRQ.getMessage());
+
+                            //check validity of new user, start by making sure that their username is unique.
+                            //This is done with the json helper's return value.
+                            JSONHelper helper = new JSONHelper();
+                            User newUser = new User(receivedRQ.getName(), receivedRQ.getPassword(),
+                                    receivedRQ.getIp(), Integer.toString(receivedRQ.getSocketNum())); //todo: check with jo if its ok if I change user.class socket to int. If so, change it
+                            if (!helper.saveNewUser(newUser)) { //if false then it tells user why
+                                System.out.println("The user already exists");
+                                server.sendObject("REGISTER-FAILED, USER ALREADY EXISTS");
+                                server.sendObject(new RQ(2, receivedRQ.getRqNum())); //todo: ask jo how to send the register failed back to the client
+                            } else {
+                                //server.sendObject("REGISTERED");
+                                System.out.println("New user added to database");
+                                RQ returnRQ = new RQ(1, receivedRQ.getRqNum()); //todo: what to do with the 1
+                                server.sendObject(returnRQ.getMessage());
+                                server.setRegistered(true);
+                            }
                         } catch (IOException e) {
-                            if(e instanceof EOFException || e instanceof SocketException){
+                            if (e instanceof EOFException || e instanceof SocketException) {
                                 System.out.println("A user disconnected while server trying to send a message");
                                 break loop;
-                            }
-                            else
-                            e.printStackTrace();
+                            } else
+                                e.printStackTrace();
                         }
                         // TODO: 2020-11-08 Add new user to the database (Use semaphore n shit)
 
                         //now that we have the data from the user we need to save it.
 
+                        break;
+
+                    case 3: //todo: add comment to say which case is what
+
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 13:
+                        break;
+                    case 17:
                         break;
 
                     default:
--- server/src/main/java/com/coen445/FinalProject/JSONHelper.java
+++ server/src/main/java/com/coen445/FinalProject/JSONHelper.java
@@ -1,81 +1,102 @@
 package com.coen445.FinalProject;
-import com.google.gson.*;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class JSONHelper {
+    private Lock lock = new ReentrantLock();
 
     //todo: update the return to return if the creation was successful or not. If not, why did it fail. Return this in the register-denied frame.
-    public void saveNewUser(User user) throws IOException {
-        writeNewUserToFile(user);
+    public boolean saveNewUser(User user) throws IOException {
+        lock.lock();
+        try {
+            return writeNewUserToFile(user);
+        } finally {
+            lock.unlock();
+        }
+
     }
 
     public void updateUser(User updatedUser) throws IOException {
-        //get current user and compare differences
+        lock.lock();
+        try {
+            //get current user and compare differences
+            //first we want to find the user we need to update
+            Gson gson = new Gson();
+            final Type USER_TYPE = new TypeToken<List<User>>() {
+            }.getType();
+            JsonReader jsonReader = new JsonReader(new FileReader("users.json"));
+            List<User> users = gson.fromJson(jsonReader, USER_TYPE);
 
-        //first we want to find the user we need to update
-        Gson gson = new Gson();
-        final Type USER_TYPE = new TypeToken<List<User>>(){}.getType();
-        JsonReader jsonReader = new JsonReader(new FileReader("users.json"));
-        List<User> users = gson.fromJson(jsonReader, USER_TYPE);
 
+            //for (User user: users) { //consider using binary search to reduce time (need to confirm if the users.json will always be in order based on ID)
+            for (int i = 0; i < users.size(); i++) {
+                if (updatedUser.getUserName().equals(users.get(i).getUserName())) {//if this is the user we want to update
+                    //for each var, check what is different starting with the most likely case
+                    //only use if because even though it can slow things down its possible that multiple things change
+                    if (!updatedUser.getInterests().equals(users.get(i).getInterests())) {
+                        users.get(i).setInterests(updatedUser.getInterests());
+                    }
+                    if (!updatedUser.getIPAddress().equals(users.get(i).getIPAddress())) {
+                        users.get(i).setIPAddress(updatedUser.getIPAddress());
+                    }
+                    if (updatedUser.getSocketNumber() != users.get(i).getSocketNumber()) {
+                        users.get(i).setSocketNumber(updatedUser.getSocketNumber());
+                    }
+                    if (!updatedUser.getUserName().equals(users.get(i).getUserName())) { //todo: this should probably be deprecated
+                        users.get(i).setUserName(updatedUser.getUserName());
+                    }
+                    //todo: is it worth updating the rest??? (DONT ALLOW UPDATING USERID!!!)
 
-        //for (User user: users) { //consider using binary search to reduce time (need to confirm if the users.json will always be in order based on ID)
-        for (int i = 0; i < users.size(); i++) {
-            if(updatedUser.getUserID() == users.get(i).getUserID()){//if this is the user we want to update
-                //for each var, check what is different starting with the most likely case
-                //only use if because even though it can slow things down its possible that multiple things change
-                if(!updatedUser.getInterests().equals(users.get(i).getInterests())){
-                    users.get(i).setInterests(updatedUser.getInterests());
+                    //upon finishing the changes break so we don't have to go through the rest
+                    break;
                 }
-                if(!updatedUser.getIPAddress().equals(users.get(i).getIPAddress())){
-                    users.get(i).setIPAddress(updatedUser.getIPAddress());
-                }
-                if(updatedUser.getSocketNumber() != users.get(i).getSocketNumber()){
-                    users.get(i).setSocketNumber(updatedUser.getSocketNumber());
-                }
-                if(!updatedUser.getUserName().equals(users.get(i).getUserName())){
-                    users.get(i).setUserName(updatedUser.getUserName());
-                }
-                //todo: is it worth updating the rest??? (DONT ALLOW UPDATING USERID!!!)
-
-                //upon finishing the changes break so we don't have to go through the rest
-                break;
             }
+            writeUpdatedUserToFile(users);
+        } finally {
+            lock.unlock();
         }
-        writeUpdatedUserToFile(users);
     }
 
     //todo: add a lock when threaded since this writes to a file
-    private void writeNewUserToFile(User newUser) throws IOException{
+    private boolean writeNewUserToFile(User newUser) throws IOException {
 
-        System.out.println("Currently saving user: " + newUser.getUserID());
+        System.out.println("Currently saving user: " + newUser.getUserName());
         /* TODO: 2020-10-17 Change the check to also see if the file is empty instead of the user,
             this is because the person might try to add a new user zero. */
-        if(newUser.getUserID() > 0) { //only do this if there are currently users in the file
+
+
+        File newFile = new File("users.json"); //use this to check if the file is empty
+
+        if (newFile.length() > 0) { //if file not empty:
 
             //Create objects we will need
             Gson gson = new Gson();
-            final Type USER_TYPE = new TypeToken<List<User>>(){}.getType();
+            final Type USER_TYPE = new TypeToken<List<User>>() {
+            }.getType();
             JsonReader jsonReader = new JsonReader(new FileReader("users.json"));
             List<User> users = gson.fromJson(jsonReader, USER_TYPE); //get all current users
             JsonArray jsonArray = new JsonArray();
 
 
             //check for duplicated. If not duplicate, add it to the list of users
-            for (User user: users) { //check duplicates
-                if(user.getUserID() == newUser.getUserID()) { //if we have a duplicate (since id is unique)
+            for (User user : users) { //check duplicates
+                if (user.getUserName().equals(newUser.getUserName())) { //if we have a duplicate (since username is unique)
                     System.out.println("This user already exists, user will not be added!");
-                    return;
-                }
-                else {
+                    return false;
+                } else {
                     jsonArray.add(gson.toJsonTree(user, User.class));
                 }
             }
@@ -92,7 +113,7 @@ public class JSONHelper {
 
             //todo: quicksort based on userID (is this even needed tho???)
 
-        } else{ //case of adding first user (where users.json is empty)
+        } else { //case of adding first user (where users.json is empty)
             Gson gson = new Gson();
             User[] userList = new User[1];
             userList[0] = newUser;
@@ -103,6 +124,7 @@ public class JSONHelper {
 
             writer.close();
         }
+        return true;
     }
 
     //todo: add a lock when threaded since this writes to a file
@@ -118,5 +140,35 @@ public class JSONHelper {
         writer.close();
     }
 
+    public boolean deleteUser(String username) throws IOException {
+        lock.lock();
+        try {
+            Gson gson = new Gson();
+            final Type USER_TYPE = new TypeToken<List<User>>() {
+            }.getType();
+            JsonReader jsonReader = new JsonReader(new FileReader("users.json"));
+            List<User> users = gson.fromJson(jsonReader, USER_TYPE);
+            JsonArray jsonArray = new JsonArray();
+            boolean retVal = false;
 
+            //check every user to see if they match. If they don't, add them to the list of users to keep. If they do, don't add them to the keep list
+            for (User user : users) {
+                if (!username.equals(user.getUserName())) {//only add the users we want to keep to the list to store again
+                    jsonArray.add(gson.toJsonTree(user, User.class));
+                } else
+                    retVal = true;
+            }
+
+            //write change to the file
+            FileWriter writer = new FileWriter("users.json", false);
+            JsonWriter jsonWriter = new JsonWriter(writer);
+            jsonWriter.setIndent(" ");
+            gson.toJson(jsonArray, jsonWriter);
+            writer.close();
+
+            return retVal;
+        } finally {
+            lock.unlock();
+        }
+    }
 }
