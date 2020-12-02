3d04c162b67139ca2b95ef8efd3e5c3b97744f3b
Brad Walsh <bradley.walsh.97@gmail.com>
2020-12-02 17:16:26 -0500

    cleaned things up a bit

--- server/src/main/java/com/coen445/FinalProject/Main.java
+++ server/src/main/java/com/coen445/FinalProject/Main.java
@@ -20,6 +20,7 @@ public class Main {
     public static ScheduledExecutorService servingTimer = Executors.newScheduledThreadPool(1);
     public static int serverPort;
     public static int altServerPort;
+    public static String altServerIP;
     public static String whichServer;
     public static boolean otherServerConnected = false;
     public static boolean backupRegistered = false;
@@ -28,11 +29,57 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         boolean servingDone = false;
+        boolean correctInput = false;
+        boolean correctPortInput = false;
+        String isPrimaryString = "";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello Systems Administrator. Is this server A or B? (A/B)");
-        whichServer = scanner.nextLine();
-        System.out.println("Is this server the primary server? (Y/N)");
-        String isPrimaryString = scanner.nextLine();
+
+        //Start by getting some initial info
+        while(!correctInput) {
+            //get user inputs
+            System.out.println("Hello Systems Administrator. Is this server A or B? (A/B)"); //used for database recognition
+            whichServer = scanner.nextLine();
+            System.out.println("Is this server the primary server? (Y/N)");
+            isPrimaryString = scanner.nextLine();
+
+            //check for correct inputs:
+            if ((whichServer.equalsIgnoreCase("a") || whichServer.equalsIgnoreCase("b"))
+            && (isPrimaryString.equalsIgnoreCase("Y") || isPrimaryString.equalsIgnoreCase("N"))){
+                correctInput = true;
+            } else {
+                System.out.println("Invalid inputs, please try again :)");
+            }
+        }
+
+        //get which port the server will listen on and then print it to the terminal.
+        serverPort = 5001;
+        while(!available(serverPort)) //get the server to run on a available port
+            serverPort++;
+        System.out.println("This server is listening on port: " + serverPort);
+
+        System.out.println("This server is running on port: " + InetAddress.getLocalHost().getHostAddress());
+
+
+        while(!correctPortInput){
+            System.out.println("Please enter the ip which the other server is running on: ");
+            altServerIP = scanner.nextLine();
+
+            //Pattern sourced from: https://stackoverflow.com/questions/5667371/validate-ipv4-address-in-java
+            String ipv4Pattern = "^((0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)\\.){3}(0|1\\d?\\d?|2[0-4]?\\d?|25[0-5]?|[3-9]\\d?)$";
+            if(altServerIP.matches(ipv4Pattern)){
+                correctInput = true;
+            } else{
+                System.out.println("Hmm, that IP address doesn't look right. Please try again");
+                continue;
+            }
+            System.out.println("Please enter the port which the other server is listening on: ");
+            altServerPort = Integer.parseInt(scanner.nextLine());
+            if(altServerPort > 5001 && altServerPort < 65535)
+                correctPortInput = true;
+            else System.out.println("Invalid port number, please try again. \n\n");
+
+        }
+
 
         boolean isPrimary;
         if(isPrimaryString.equalsIgnoreCase("Y")) {
@@ -52,15 +99,15 @@ public class Main {
         //ServerSocket listener = null;
         //Socket otherServerSocket = null;
         //if(available(serverPort))
-        if(whichServer.equalsIgnoreCase("a")) {
-            serverPort = ServerInfo.SERVER_A_PORT;
-            //listener = new ServerSocket(serverPort);
-            altServerPort = ServerInfo.SERVER_B_PORT;
-        }else if(whichServer.equalsIgnoreCase("b")){
-            serverPort = ServerInfo.SERVER_B_PORT;
-            //listener = new ServerSocket(serverPort);
-            altServerPort = ServerInfo.SERVER_A_PORT;
-        }
+//        if(whichServer.equalsIgnoreCase("a")) {
+//            serverPort = ServerInfo.SERVER_A_PORT;
+//            //listener = new ServerSocket(serverPort);
+//            altServerPort = ServerInfo.SERVER_B_PORT;
+//        }else if(whichServer.equalsIgnoreCase("b")){
+//            serverPort = ServerInfo.SERVER_B_PORT;
+//            //listener = new ServerSocket(serverPort);
+//            altServerPort = ServerInfo.SERVER_A_PORT;
+//        }
 
         //now that the server has been created, start a timer between 3 & 5 minutes
         Random randTimerValue = new Random();
--- server/src/main/java/com/coen445/FinalProject/ServerInfo.java
+++ server/src/main/java/com/coen445/FinalProject/ServerInfo.java
@@ -1,8 +1,8 @@
 package com.coen445.FinalProject;
 
 public abstract class ServerInfo{
-    public static final String SERVER_A_ADDRESS = "localhost";
-    public static final String SERVER_B_ADDRESS = "localhost";
-    public static final int SERVER_A_PORT = 5001;
-    public static final int SERVER_B_PORT = 5002;
+    public static String SERVER_A_ADDRESS = "localhost";
+    public static String SERVER_B_ADDRESS = "localhost";
+    public static int SERVER_A_PORT = 5001;
+    public static int SERVER_B_PORT = 5002;
 }
