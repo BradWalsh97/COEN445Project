f6e7b91876c09df62d0edb600e2bfcd4d8c099a1
Brad Walsh <bradley.walsh.97@gmail.com>
2020-12-02 14:09:22 -0500

    added login to user class

--- server/src/main/java/com/coen445/FinalProject/User.java
+++ server/src/main/java/com/coen445/FinalProject/User.java
@@ -14,6 +14,7 @@ public class User implements Serializable {
 
     private String socketNumber;
     private boolean isDeleted;
+    private boolean loggedIn;
 
     User(String Name, String Password, String IPAddress, String socket){
         this.userID = User.nextID;
@@ -24,8 +25,10 @@ public class User implements Serializable {
         this.isDeleted = false;
         this.IPAddress = IPAddress;
         this.socketNumber = socket;
+        this.loggedIn = true; //since user is logging in my registering
     }
-
+    public boolean getLoggedIn() {return loggedIn;}
+    public void setLoggedIn(boolean logStat) {this.loggedIn = logStat;}
     public String getIPAddress() {
         return IPAddress;
     }
